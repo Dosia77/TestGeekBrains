62684a9987395d6acc59d89f2dcf2d0a58d7f15f
Dosia77 <125308879+Dosia77@users.noreply.github.com>
2023-04-08 02:35:35 +0300

    added changes

--- GeerBrainsTest/GeekTest.cs
+++ GeerBrainsTest/GeekTest.cs
@@ -8,12 +8,40 @@ namespace GeerBrainsTest
 {
     internal class GeekTest
     {
+        static string[] NewArray(string[] array1)
+        {
+            string[] array2 = new string[array1.Length];
+            int count = 0;
+            for (int i = 0; i < array1.Length; i++)
+            {
+                if (array1[i].Length <= 3)
+                {
+                    array2[count] = array1[i];
+                    count++;
+                }
+            }
+            return array2;
+        }
+        static void PrintArray(string[] array)
+        {
+            Console.Write("[");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+            Console.Write("]");
+
+            Console.WriteLine();
+        }
+
+
         static void Main(string[] args)
         {
             string[] arr1 = new string[9] { "Gee", "kBr", "ain", "s", "20", "23", "Hello", "world", "!" };
             string[] arr2 = NewArray(arr1);
             PrintArray(arr1);
             PrintArray(arr2);
+            Console.Readline();
 
         }
     }
