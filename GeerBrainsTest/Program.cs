731245f7335df0e868f0dcfd835c7decbf5174e9
Dosia77 <125308879+Dosia77@users.noreply.github.com>
2023-04-08 00:11:00 +0300

    Add files via upload
    added file

---
+++ GeerBrainsTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeerBrainsTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr1 = new string[9] { "Gee", "kBr", "ain", "s", "20", "23", "Hello", "world", "!" };
+            string[] arr2 = NewArray(arr1);
+            PrintArray(arr1);
+            PrintArray(arr2);
+
+        }
+    }
+}
